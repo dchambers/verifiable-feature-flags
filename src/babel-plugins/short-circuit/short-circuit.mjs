8ff93eb26537a2e2446ba4079375821101eb6749
Dominic Chambers <dominic.chambers@babylonhealth.com>
2020-12-17 22:41:42 +0000

    Working Short Circuit Plugin

--- src/babel-plugins/short-circuit/short-circuit.mjs
+++ src/babel-plugins/short-circuit/short-circuit.mjs
@@ -1,7 +1,47 @@
-const shortCircuitPlugin = (babel) => ({
+const isEmptyArraySpread = (node) =>
+  node.type === 'SpreadElement' &&
+  node.argument.type === 'ArrayExpression' &&
+  node.argument.elements.length === 0
+
+const isEmptyObjectSpread = (node) =>
+  node.type === 'SpreadElement' &&
+  node.argument.type === 'ObjectExpression' &&
+  node.argument.properties.length === 0
+
+const isLiteralTrue = (node) =>
+  node.type === 'BooleanLiteral' && node.value === true
+
+const isLiteralFalse = (node) =>
+  node.type === 'BooleanLiteral' && node.value === false
+
+const shortCircuitPlugin = () => ({
   visitor: {
-    // TODO...
-  }
+    ArrayExpression: (path) => {
+      path.node.elements = path.node.elements.filter(
+        (node) => !isEmptyArraySpread(node)
+      )
+    },
+    ObjectExpression: (path) => {
+      path.node.properties = path.node.properties.filter(
+        (node) => !isEmptyObjectSpread(node)
+      )
+    },
+    LogicalExpression: (path) => {
+      if (path.node.operator === '&&') {
+        if (isLiteralTrue(path.node.left)) {
+          Object.assign(path.node, path.node.right)
+        } else if (isLiteralTrue(path.node.right)) {
+          Object.assign(path.node, path.node.left)
+        }
+      } else if (path.node.operator === '||') {
+        if (isLiteralFalse(path.node.left)) {
+          Object.assign(path.node, path.node.right)
+        } else if (isLiteralFalse(path.node.right)) {
+          Object.assign(path.node, path.node.left)
+        }
+      }
+    },
+  },
 })
 
 export default shortCircuitPlugin
--- src/babel-plugins/short-circuit/short-circuit.test.mjs
+++ src/babel-plugins/short-circuit/short-circuit.test.mjs
@@ -3,35 +3,48 @@ import babel from '@babel/core'
 
 import shortCircuitPlugin from './index.mjs'
 
-const transpile = (sourceCode, plugins = []) => babel.transform(sourceCode, { plugins }).code.split('\n')
+const transpile = (sourceCode, plugins = []) =>
+  babel.transform(sourceCode, { plugins }).code.split('\n')
 
-tap.test('short circuitable code is simplified', test => {
+tap.test('short circuitable code is simplified (1)', (test) => {
   const sourceCode = `
-    const arr = [1, 2, 3, ...[]]
-    const obj = {key: 'val', ...{}}
-    const andBool = value1 && true
-    const orBool = value1 || false
+    const arr = [1, ...[], 2, 3, ...[]]
+    const obj = {...{}, key: 'val', ...{}}
+    const andLeftLiteralBool = true && value1
+    const andRightLiteralBool = value1 && true
+    const orLeftLiteralBool = false || value1
+    const orRightLiteralBool = value1 || false
   `
 
   const targetCode = `
     const arr = [1, 2, 3]
     const obj = {key: 'val'}
-    const andBool = value1
-    const orBool = value1
+    const andLeftLiteralBool = value1
+    const andRightLiteralBool = value1
+    const orLeftLiteralBool = value1
+    const orRightLiteralBool = value1
   `
 
-  test.deepEquals(transpile(sourceCode, [shortCircuitPlugin]), transpile(targetCode))
+  test.deepEquals(
+    transpile(sourceCode, [shortCircuitPlugin]),
+    transpile(targetCode)
+  )
   test.end()
 })
 
-tap.test('non short circuitable code is left unchanged', test => {
+tap.test('non short circuitable code is left unchanged', (test) => {
   const sourceCode = `
-    const arr = [1, 2, 3, ...[4]]
+    const arr = [1, ...[2], 3, ...[4]]
     const obj = {key: 'val', ...{key2: 'val2'}}
-    const andBool = value1 && false
-    const orBool = value1 || true
+    const andLeftLiteralBool = false && value1
+    const andRightLiteralBool = value1 && false
+    const orLeftLiteralBool = true || value1
+    const orRightLiteralBool = value1 || true
   `
 
-  test.deepEquals(transpile(sourceCode, [shortCircuitPlugin]), transpile(sourceCode))
+  test.deepEquals(
+    transpile(sourceCode, [shortCircuitPlugin]),
+    transpile(sourceCode)
+  )
   test.end()
 })

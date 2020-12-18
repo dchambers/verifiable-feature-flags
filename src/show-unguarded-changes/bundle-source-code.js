172c3103a7c8da190a9094f0df4e8c073da57577
Dominic Chambers <dominic.chambers@babylonhealth.com>
2020-12-18 14:28:21 +0000

    Implement `bundleSourceCode` function

--- src/show-unguarded-changes/bundle-source-code.js
+++ src/show-unguarded-changes/bundle-source-code.js
@@ -1,3 +1,14 @@
-const bundleSourceCode = (src) => {}
+import { rollup } from 'rollup'
+
+const bundleSourceCode = async (src) => {
+  const bundle = await rollup({
+    input: src,
+  })
+  const { output } = await bundle.generate({})
+
+  await bundle.close()
+
+  return output[0].code
+}
 
 export default bundleSourceCode
